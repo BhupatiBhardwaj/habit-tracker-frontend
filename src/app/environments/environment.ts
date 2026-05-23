e4fa56c0394fd1d3b862d6953fbfcd7ffca45b4b
Bhupati Bhardwaj <bhupatibhardwaj83@gmail.com>
2026-05-23 19:50:18 +0530

    changed the backend URL

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7211/api'
-  //apiUrl: 'https://habit-tracker-api-mqws.onrender.com/api'
+  //apiUrl: 'https://localhost:7211/api'
+  apiUrl: 'https://habit-tracker-api-mqws.onrender.com/api'
 };
